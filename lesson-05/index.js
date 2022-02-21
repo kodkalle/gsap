3980f745ecd081f83dfc4f872d52fc1b7bc7c4dd
Karl Berg <kalle@kodknack.com>
2022-02-21 12:18:13 +0100

    enable the player to interact with buttons

--- lesson-05/index.js
+++ lesson-05/index.js
@@ -1,18 +1,21 @@
 const player = document.getElementById("player");
 const monster = document.getElementById("monster");
+const button = document.getElementById("button");
 
+let MONSTER_IS_LOOSE = false;
+
+// offset the pointer to be centered within the circle
 gsap.set(player, { xPercent: -50, yPercent: -50 });
-// set start position;
 gsap.set(monster, { xPercent: -50, yPercent: -50 });
 
 //starting position
 const playerPos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-const monsterPos = { x: 0, y: 0};
+const monsterPos = { x: 0, y: 0 };
 
 const mouse = { x: playerPos.x, y: playerPos.y };
 
 const playerSpeed = 1; // slows down
-const monsterSpeed = 0.01; // slows down
+const monsterSpeed = 0.05; // slows down
 
 const xSetPlayer = gsap.quickSetter(player, "x", "px");
 const ySetPlayer = gsap.quickSetter(player, "y", "px");
@@ -25,7 +28,17 @@ window.addEventListener("mousemove", (e) => {
   mouse.y = e.y;
 });
 
-window.addEventListener("keydown", () => animateMonster());
+button.onclick = () => {
+  if (MONSTER_IS_LOOSE) {
+    gsap.ticker.remove(monsterPosition);
+    button.innerText = 'Set the Monster loose!';
+    MONSTER_IS_LOOSE = false;
+  } else {
+    gsap.ticker.add(monsterPosition);
+    button.innerText = 'Freeze the Monster!';
+    MONSTER_IS_LOOSE = true;
+  }
+};
 
 const speedAdjuster = (speed) => {
   let dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
@@ -41,13 +54,9 @@ gsap.ticker.add(() => {
   ySetPlayer(playerPos.y);
 });
 
-const animateMonster = () => {
-  console.log('Release the kraken!', gsap.ticker)
-  gsap.ticker.add(() => {
+const monsterPosition = () => {
     monsterPos.x += (mouse.x - monsterPos.x) * speedAdjuster(monsterSpeed);
     monsterPos.y += (mouse.y - monsterPos.y) * speedAdjuster(monsterSpeed);
-
     xSetMonster(monsterPos.x);
     ySetMonster(monsterPos.y);
-  });
-};
+}
